326b78baab62c21979a3100dce6ed3ca486565e4
VictorMachado38 <54145667+VictorMachado38@users.noreply.github.com>
2021-10-26 10:59:09 -0300

    Melhoria

--- AtividadeProcessual/UDP-Cliente- Conversão de moeda/src/com/SitemasDistribuidos/Main.java
+++ AtividadeProcessual/UDP-Cliente- Conversão de moeda/src/com/SitemasDistribuidos/Main.java
@@ -1,28 +1,33 @@
 package com.SitemasDistribuidos;
 
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-         moedaRealParaDolar moeda = new moedaRealParaDolar();
+        moedaRealParaDolar moeda = new moedaRealParaDolar();
         ClienteUDP clienteUDP = new ClienteUDP();
-       // ClienteUDP1 ClienteUDP1 = new ClienteUDP1();
+        Scanner scanner = new Scanner(System.in);
 
-        moeda.setNome("Conversão de real para DOLAR");
-        moeda.setValorDeReias(100.00);
-        moeda.setValorAtualDoDolar(5.55);
-        moeda.setValorCONVERTIDO(0.0);
+        System.out.println("Qual é a descrição da conversão ?");
+        moeda.setDescricao(scanner.nextLine());
+        System.out.println("Qual o valor de reais(R$) que voce quer converter para dolar(US$) ? Digite o valor com ("+ "," + ") por ex 984,82.");
+        moeda.setValorDeReias(scanner.nextDouble());
+        System.out.println("Qual a cotação do dolar(US$) atual? Digite o valor com ("+ "," + ") por ex 984,82.");
+        moeda.setValorAtualDoDolar(scanner.nextDouble());
 
 
 
         moeda = clienteUDP.enviarDadaosParaconvesao(moeda);
 
-        System.out.println("Nome: "+moeda.getNome());
+        System.out.println("Descrição: "+moeda.getDescricao());
         System.out.println("Valor de reais: R$"+moeda.getValorDeReias());
-        System.out.println("Valor atual do dolar: "+moeda.getValorAtualDoDolar());
+        System.out.println("Valor atual do dolar US$: "+moeda.getValorAtualDoDolar());
         System.out.println("----------------------------------------------------------");
-        System.out.println("Valor da conversão:");
-        System.out.println("Foram comvertidos R$"+moeda.getValorDeReias() +"para dolar, com o doler custando: US$"+moeda.getValorAtualDoDolar() +"a CONVERSÃO DEU: US$" + moeda.getValorCONVERTIDO());
+        System.out.println("Valor da conversão: US$" + moeda.getValorCONVERTIDO());
+
 	// write your code here
     }
 }
