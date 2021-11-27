8fbb91cb8a08692bc633e24b61575181b6764ae9
VictorMachado38 <54145667+VictorMachado38@users.noreply.github.com>
2021-11-27 17:01:39 -0300

    Chat (Atividade avaliativa processual N2) - Interface 1.0

---
+++ AtividadeProcessual-N2/chat/src/com/SitemasDistribuidos/Main.java
@@ -0,0 +1,12 @@
+package com.SitemasDistribuidos;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.printf("Teste");
+
+    }
+
+}
